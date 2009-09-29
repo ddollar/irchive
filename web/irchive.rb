7ada1915ef7ad642b75a3cdce7cdf55c2f45fa39
David Dollar <ddollar@gmail.com>
2009-09-28 21:58:33 -0400

    censor emails

--- web/irchive.rb
+++ web/irchive.rb
@@ -75,10 +75,13 @@ def channels
 end
 
 def format_message(message)
-  message = CGI::escapeHTML(message)
   message = message.gsub(%r{https?://([-\w\.]+)+(:\d+)?(/([\w/_\-\%\.]*(\?\S+)?)?)?}) do |url|
     %{ <a href="#{url}" rel="nofollow">#{url}</a> }
   end
+  message = message.gsub(/([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+/) do |email|
+    %{ [redacted] }
+  end
+  message = CGI::escapeHTML(message)
 end
 
 def message(id)
